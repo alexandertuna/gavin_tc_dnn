2901110499dee6a62426535267542beb0da6c09f
alexandertuna <alexander.tuna@gmail.com>
2025-06-10 17:00:52 -0700

    Speed test over all events as a test

--- python/main.py
+++ python/main.py
@@ -4,25 +4,26 @@ from train import Trainer
 def main():
     file_path = "/Users/alexandertuna/Downloads/cms/gavin_tc_dnn/data/pls_t5_embed.root"
     processor = Preprocessor(file_path)
-    trainer = Trainer(processor.X_left_train,
-                      processor.X_left_test,
-                      processor.X_right_train,
-                      processor.X_right_test,
-                      processor.y_t5_train,
-                      processor.y_t5_test,
-                      processor.w_t5_train,
-                      processor.w_t5_test,
-                      # --------------------
-                      processor.X_pls_train,
-                      processor.X_pls_test,
-                      processor.X_t5raw_train,
-                      processor.X_t5raw_test,
-                      processor.y_pls_train,
-                      processor.y_pls_test,
-                      processor.w_pls_train,
-                      processor.w_pls_test,
-                      )
-    trainer.train()
+    processor.speed_test()
+    # trainer = Trainer(processor.X_left_train,
+    #                   processor.X_left_test,
+    #                   processor.X_right_train,
+    #                   processor.X_right_test,
+    #                   processor.y_t5_train,
+    #                   processor.y_t5_test,
+    #                   processor.w_t5_train,
+    #                   processor.w_t5_test,
+    #                   # --------------------
+    #                   processor.X_pls_train,
+    #                   processor.X_pls_test,
+    #                   processor.X_t5raw_train,
+    #                   processor.X_t5raw_test,
+    #                   processor.y_pls_train,
+    #                   processor.y_pls_test,
+    #                   processor.w_pls_train,
+    #                   processor.w_pls_test,
+    #                   )
+    # trainer.train()
 
 if __name__ == "__main__":
     main()
