6be0b196627733954578ff16d90bd0624e4349d6
alexandertuna <alexander.tuna@gmail.com>
2025-08-24 22:32:02 -0700

    Script for making plots with diff dr2

---
+++ experiments/phi_dependence_on_dr2/train.sh
@@ -0,0 +1,16 @@
+for DR2 in 0.02 0.05 0.1 0.2 0.4 0.6 0.8 1.0; do
+
+    DIR="dr2_norm_${DR2}"
+    mkdir -p ${DIR}
+    cd ${DIR}
+    # time python ../../../python/main.py --delta_r2_cut ${DR2}
+    cd -
+
+    DIR="dr2_proj_${DR2}"
+    mkdir -p ${DIR}
+    cd ${DIR}
+    # time python ../../../python/main.py --delta_r2_cut ${DR2} --use_phi_projection
+    cd -
+
+done
+
