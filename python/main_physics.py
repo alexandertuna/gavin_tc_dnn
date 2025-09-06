a1bdb4f484ec0c94095e53b2a8935d82e680d420
alexandertuna <alexander.tuna@gmail.com>
2025-09-05 19:02:55 -0700

    Start adding training

--- python/main_physics.py
+++ python/main_physics.py
@@ -4,7 +4,7 @@ Main script to run Gavin's track embedding model training and visualization
 import argparse
 
 from preprocess import PreprocessorPtEtaPhi
-from train import Trainer
+from train import TrainerPtEtaPhi
 from viz import Plotter
 from pathlib import Path
 
@@ -28,6 +28,8 @@ def options():
                         help="Number of epochs for training the model")
     parser.add_argument("--seed", type=int, default=42,
                         help="Random seed for reproducibility")
+    parser.add_argument("--use_scheduler", action="store_true",
+                        help="Use learning rate scheduler during training")
     parser.add_argument("--upweight_displaced", type=float, default=5.0,
                         help="Upweight factor for displaced features")
     parser.add_argument("--test_size", type=float, default=0.2,
@@ -53,35 +55,25 @@ def main():
                                      recalculate_std=args.recalculate_std,
                                      )
 
-    return
 
     # ML training
-    trainer = Trainer(seed=args.seed,
-                      num_epochs=args.num_epochs,
-                      emb_dim=args.emb_dim,
-                      use_pls_deltaphi=args.use_pls_deltaphi,
-                      use_scheduler=args.use_scheduler,
-                      # --------------------
-                      bonus_features=processor.bonus_features,
-                      # --------------------
-                      X_left_train=processor.X_left_train,
-                      X_left_test=processor.X_left_test,
-                      X_right_train=processor.X_right_train,
-                      X_right_test=processor.X_right_test,
-                      y_t5_train=processor.y_t5_train,
-                      y_t5_test=processor.y_t5_test,
-                      w_t5_train=processor.w_t5_train,
-                      w_t5_test=processor.w_t5_test,
-                      # --------------------
-                      X_pls_train=processor.X_pls_train,
-                      X_pls_test=processor.X_pls_test,
-                      X_t5raw_train=processor.X_t5raw_train,
-                      X_t5raw_test=processor.X_t5raw_test,
-                      y_pls_train=processor.y_pls_train,
-                      y_pls_test=processor.y_pls_test,
-                      w_pls_train=processor.w_pls_train,
-                      w_pls_test=processor.w_pls_test,
-                      )
+    trainer = TrainerPtEtaPhi(seed=args.seed,
+                              num_epochs=args.num_epochs,
+                              use_scheduler=args.use_scheduler,
+                              # --------------------
+                              bonus_features=processor.bonus_features,
+                                # --------------------
+                              features_t5_train=processor.features_t5_train,
+                              features_t5_test=processor.features_t5_test,
+                              features_pls_train=processor.features_pls_train,
+                              features_pls_test=processor.features_pls_test,
+                              sim_features_t5_train=processor.sim_features_t5_train,
+                              sim_features_t5_test=processor.sim_features_t5_test,
+                              sim_features_pls_train=processor.sim_features_pls_train,
+                              sim_features_pls_test=processor.sim_features_pls_test,
+                              )
+    
+    return 
 
     if not args.load_model:
         trainer.train()
