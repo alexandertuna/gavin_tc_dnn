4797581b364465dbbee95faf3b040c1a601c8a92
alexandertuna <alexander.tuna@gmail.com>
2025-09-06 18:39:26 -0700

    Add flexibility to make roc curve comparisons with default embedding

--- python/main_physics.py
+++ python/main_physics.py
@@ -3,9 +3,9 @@ Main script to run Gavin's track embedding model training and visualization
 """
 import argparse
 
-from preprocess import PreprocessorPtEtaPhi
-from train import TrainerPtEtaPhi
-from viz import Plotter
+from preprocess import Preprocessor, PreprocessorPtEtaPhi
+from train import Trainer, TrainerPtEtaPhi
+from viz import PlotterPtEtaPhi
 from pathlib import Path
 
 
@@ -20,9 +20,11 @@ def options():
                         help="Path to the precomputed T5 sim features file")
     parser.add_argument("--sim_features_pls", type=str, default=f"{default_dir}/sim_features_pls.pkl",
                         help="Path to the precomputed PLS sim features file")
-    parser.add_argument("--model", type=str, default="model_weights.pth",
+    parser.add_argument("--model", type=str, default="model_weights_ptetaphi.pth",
                         help="Path to save or load the model weights")
-    parser.add_argument("--pdf", type=str, default="plots.pdf",
+    parser.add_argument("--embedding_model", type=str, default="model_weights.pth",
+                        help="Path to save or load the Gavin embedding model weights")
+    parser.add_argument("--pdf", type=str, default="plots_ptetaphi.pdf",
                         help="Path to save the output plots in PDF format")
     parser.add_argument("--num_epochs", type=int, default=200,
                         help="Number of epochs for training the model")
@@ -36,6 +38,10 @@ def options():
                         help="Test size for train-test split")
     parser.add_argument("--recalculate_std", action="store_true",
                         help="Recalculate standard deviation from the data instead of using predefined values")
+    parser.add_argument("--load_model", action="store_true",
+                        help="Load model weights from the specified path instead of training")
+    parser.add_argument("--load_embedding_model", action="store_true",
+                        help="Load Gavin embedding model weights from the specified path instead of training")
     return parser.parse_args()
 
 
@@ -72,8 +78,6 @@ def main():
                               sim_features_pls_train=processor.sim_features_pls_train,
                               sim_features_pls_test=processor.sim_features_pls_test,
                               )
-    
-    return 
 
     if not args.load_model:
         trainer.train()
@@ -83,7 +87,57 @@ def main():
     else:
         trainer.load(model_path)
 
-    plotter = Plotter(trainer)
+    # Load default embedding model for ROC curve comparison
+    train_emb = None
+    if args.load_embedding_model:
+        print(f"Loading Gavin embedding model weights from {args.embedding_model}")
+        proc_emb = Preprocessor(root_path=None,
+                                LOAD_FEATURES=True,
+                                LOAD_PAIRS=True,
+                                FEATURES_T5="features_t5.pkl",
+                                FEATURES_PLS="features_pls.pkl",
+                                PAIRS_T5T5="pairs_t5t5.pkl",
+                                PAIRS_T5PLS="pairs_t5pls.pkl",
+                                PAIRS_PLSPLS="pairs_plspls.pkl",
+                                use_phi_projection=False,
+                                use_phi_plus_pi=False,
+                                use_pls_deltaphi=False,
+                                use_no_phi=False,
+                                upweight_displaced=5.0,
+                                delta_r2_cut=0.02,
+                                test_size=0.2,
+                                dont_swap_lr=False,
+                                )
+        train_emb = Trainer(seed=None,
+                            num_epochs=None,
+                            emb_dim=6,
+                            use_pls_deltaphi=None,
+                            use_scheduler=args.use_scheduler,
+                            # --------------------
+                            bonus_features=proc_emb.bonus_features,
+                            # --------------------
+                            X_left_train=proc_emb.X_left_train,
+                            X_left_test=proc_emb.X_left_test,
+                            X_right_train=proc_emb.X_right_train,
+                            X_right_test=proc_emb.X_right_test,
+                            y_t5_train=proc_emb.y_t5_train,
+                            y_t5_test=proc_emb.y_t5_test,
+                            w_t5_train=proc_emb.w_t5_train,
+                            w_t5_test=proc_emb.w_t5_test,
+                            # --------------------
+                            X_pls_train=proc_emb.X_pls_train,
+                            X_pls_test=proc_emb.X_pls_test,
+                            X_t5raw_train=proc_emb.X_t5raw_train,
+                            X_t5raw_test=proc_emb.X_t5raw_test,
+                            y_pls_train=proc_emb.y_pls_train,
+                            y_pls_test=proc_emb.y_pls_test,
+                            w_pls_train=proc_emb.w_pls_train,
+                            w_pls_test=proc_emb.w_pls_test,
+                            )
+        train_emb.load(args.embedding_model)
+
+
+    plotter = PlotterPtEtaPhi(trainer, train_emb)
     plotter.plot(pdf_path)
 
     # inspector = InspectT5T5(processor, trainer)
