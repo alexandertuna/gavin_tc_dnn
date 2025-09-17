58abfcb5a33ef0b5e8a1f0a70515e377f65a7113
alexandertuna <alexander.tuna@gmail.com>
2025-09-17 12:03:35 -0700

    Add a basic condor submit situation

---
+++ condor/testing/README.md
@@ -0,0 +1,5 @@
+```
+mkdir -p output/ log/ error/
+condor_submit test.sub
+condor_watch_q
+```
---
+++ condor/testing/hello_world.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+echo "whoami"
+whoami
+
+echo "pwd"
+pwd
+
+echo "hostname"
+hostname
+
+echo "ls /"
+ls /
+
+echo "ls /ceph"
+ls /ceph
+
+echo "ls /ceph/users/atuna"
+ls /ceph/users/atuna
+
+echo "ls ./"
+ls ./
+
+echo "env"
+env
+
+echo "touch file.txt"
+touch file.txt
