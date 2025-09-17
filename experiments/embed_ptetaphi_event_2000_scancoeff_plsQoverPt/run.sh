a99defac2bfe590b351a6e4e53e2f89121e2266a
alexandertuna <alexander.tuna@gmail.com>
2025-09-17 11:00:38 -0700

    Loop this bad boi

--- experiments/embed_ptetaphi_event_2000_scancoeff_plsQoverPt/run.sh
+++ experiments/embed_ptetaphi_event_2000_scancoeff_plsQoverPt/run.sh
@@ -1,6 +1,16 @@
-EPOCHS=200
-C_QPT=1.0
-C_ETA=1.0
-C_PHI=1.0
-MODEL="model_weights_ptetaphi_${C_QPT}_${C_ETA}_${C_PHI}.pth"
-time python ../../python/main_physics.py --model ${MODEL} --c_qpt ${C_QPT} --c_eta ${C_ETA} --c_phi ${C_PHI} --num_epochs ${EPOCHS} | tee log_physics_${C_QPT}_${C_ETA}_${C_PHI}.txt
+EPOCHS=50
+# C_QPT=1.0
+# C_ETA=1.0
+# C_PHI=1.0
+for C_QPT in 0.25 0.5 1.0 2.0 4.0; do
+    for C_ETA in 0.25 0.5 1.0 2.0 4.0; do
+        for C_PHI in 0.25 0.5 1.0 2.0 4.0; do
+
+            MODEL="model_weights_ptetaphi_${C_QPT}_${C_ETA}_${C_PHI}.pth"
+
+            time python ../../python/main_physics.py --model ${MODEL} --c_qpt ${C_QPT} --c_eta ${C_ETA} --c_phi ${C_PHI} --num_epochs ${EPOCHS} | tee log_physics_${C_QPT}_${C_ETA}_${C_PHI}.txt
+
+        done
+    done
+done
+
